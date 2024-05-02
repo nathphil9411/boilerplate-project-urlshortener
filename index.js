a2a6c262c43fa1b670d0391211ca7ce530197ce5
Ezeanaka Nathaniel Nwabueze <ezeanakanath@gmail.com>
2024-05-02 11:04:43 +0100

    Final solution

--- index.js
+++ index.js
@@ -1,24 +1,75 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+	res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+let url = process.env.DATABASE;
+mongoose
+	.connect(url)
+	.then(() => console.log("connected to db"))
+	.catch((err) => {
+		console.log(err);
+	});
+
+app.listen(port, function () {
+	console.log(`Listening on port ${port}`);
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+//mongoose schema
+const urlschema = new mongoose.Schema({
+	original_url: { type: String, required: true },
+	short_url: Number,
 });
+
+const Url = mongoose.model("Url", urlschema);
+
+//Url Matcher function
+const matchUrl = async (req, res) => {
+	try {
+		const shortUrl = parseInt(req.params.shorturl);
+		const urlObject = await Url.findOne({ short_url: shortUrl });
+		const originalUrl = urlObject.original_url;
+		res.redirect(originalUrl);
+	} catch (err) {
+		res.json({ error: "url not found" });
+	}
+};
+
+//Url shortner function
+const shortenUrl = async (req, res) => {
+	try {
+		const originalUrl = req.body.url;
+		let shortUrl = 0;
+		const lastUrl = await Url.findOne({}).sort({ short_url: "desc" });
+		if (!lastUrl) shortUrl++;
+		if (lastUrl) shortUrl = lastUrl.short_url + 1;
+		const newUrl = await Url.create({
+			original_url: originalUrl,
+			short_url: shortUrl,
+		});
+		console.log(newUrl);
+		res.json(newUrl);
+	} catch (err) {
+		res.json({ error: "invalid url" });
+	}
+};
+
+app.get("/api/shorturl/:shorturl", matchUrl);
+app.post(
+	"/api/shorturl/",
+	bodyParser.urlencoded({ extended: false }),
+	shortenUrl
+);
